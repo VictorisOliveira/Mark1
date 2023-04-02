045c20149321a67cfd2cf5e69a35776a6f861ae7
Victor I S Oliveira <70918081+VictorisOliveira@users.noreply.github.com>
2023-04-02 19:06:04 -0300

    Add files via upload

---
+++ house.py
@@ -0,0 +1,11 @@
+name = input("Whats´s your name?  ")
+if name== "Harry":
+    print(f"{name} is Gryffindor")
+elif name== "hermione":
+    print(f"{name} is Gryffindor")
+elif name== "Ron":
+    print(f"{name} is Gryffindor")
+elif name== "Draco":
+    print(f"{name} is Slytherin")
+else:
+    print("Who?")
